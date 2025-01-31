d412ab70dbb1b7971853b1ee15c06a94e0b54024
AhmedMashta7Nadim <ahmednadimmashta7@gmail.com>
2025-01-31 04:40:27 -0800

    v-4

--- Damascus/Controllers/CourseController.cs
+++ Damascus/Controllers/CourseController.cs
@@ -3,8 +3,10 @@ using InfraStractur.RepositoryModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Model_Entity.DTO;
 using Model_Entity.Models;
+using Model_Entity.VM;
 
 namespace Damascus.Controllers
 {
@@ -34,6 +36,22 @@ namespace Damascus.Controllers
             return Ok(request);
         }
 
+        [HttpGet("getAllCourses")]
+        public async Task<ActionResult<List<CourseSummary>>> getCoursesAsync()
+        {
+            var getAll = await courseRepository.GetAllAsync();
+            return Ok(getAll);
+        }
 
+        [HttpGet("GetCourseAsync")]
+        public async Task<ActionResult<object>> GetCourseAsync(Guid id)
+        {
+            var get = await courseRepository.GetAsync(id, true, x => x.Include(u=>u.users));
+            if (get is null)
+            {
+                return NotFound();
+            }
+            return Ok(get);
+        }
     }
 }
